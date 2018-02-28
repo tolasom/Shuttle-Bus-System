233483c407217e72c238e94952620cc3f0085ad4
Dy Sopheak <sopheakdy23@gmail.com>
2018-02-28 16:46:22 +0700

    commit

--- src/main/java/com/ServiceClasses/Customer_Service_Imp.java
+++ src/main/java/com/ServiceClasses/Customer_Service_Imp.java
@@ -6,7 +6,7 @@ import com.DaoClasses.Custom_Dao;
 
 public class Customer_Service_Imp {
 	Custom_Dao custom_Dao;
-	public Map<Object, String> user_info(){
-		return custom_Dao.user_info();
-	}
+//	public Map<Object, String> user_info(){
+//		return custom_Dao.user_info();
+//	}
 }
